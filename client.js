43feadbd960f9d3c6d553b4724ccc9468d2d892c
Donnie Walsh <donfather1000@gmail.com>
2021-09-22 22:14:08 +0000

    added key handler

--- client.js
+++ client.js
@@ -10,12 +10,7 @@ const connect = function () {
     console.log("Connected to server");
     conn.write("Name: LOL");
   });
-//----------------------------------------
 
-// "Move: up" - move up one square (unless facing down)
-// "Move: down" - move down one square (unless facing up)
-// "Move: left" - move left one square (unless facing right)
-// "Move: right" - move left one square (unless facing left)
 
 
   conn.on("data", (data) => {
--- play.js
+++ play.js
@@ -1,8 +1,28 @@
 const net = require("net");
 const client = require("./client");
-
-
 client.connect();
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', (key) => {
+    if (key === '\u0003'){
+
+      process.exit(0);
+    }
+  });
+
+  return stdin;
+
+}
+
+setupInput();
+
+
+
 
 // "Move: up" - move up one square (unless facing down)
 // "Move: down" - move down one square (unless facing up)
