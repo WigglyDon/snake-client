13f9dbf8b695fd61a4ade3bbd0ab84441c4934de
Donnie Walsh <donfather1000@gmail.com>
2021-09-22 20:51:04 +0000

    separated client object to it's own file and imported it into play

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
 const net = require("net");
+const client = require("./client");
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',
-    port: '50542'
-  });
-  conn.setEncoding("utf8");
 
-  conn.on("connect", () => {
-    console.log("Connecting ...");
-  });
-
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  
-
-  return conn;
-};
-
-connect();
+client.connect();
