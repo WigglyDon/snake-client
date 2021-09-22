c9fc0aaff49e87e9df9bb5da9940df12e602463f
Donnie Walsh <donfather1000@gmail.com>
2021-09-22 22:19:09 +0000

    moved setupInput to its own input file

--- play.js
+++ play.js
@@ -1,25 +1,10 @@
 const net = require("net");
 const client = require("./client");
+const input = require("./input");
+
+
 client.connect();
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', (key) => {
-    if (key === '\u0003'){
-
-      process.exit(0);
-    }
-  });
-
-  return stdin;
-
-}
-
-setupInput();
+input.setupInput();
 
 
 
