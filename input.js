b9d72e6f80e2cc4b66ca205077f21cabfa8e2da6
Donnie Walsh <donfather1000@gmail.com>
2021-09-23 01:19:01 +0000

    finished the client for snake game

--- input.js
+++ input.js
@@ -1,24 +1,35 @@
+let connection;
 
-
-
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on('data', (key) => {
-    if (key === '\u0003'){
-
-      process.exit(0);
-    }
-  });
-
+  stdin.on('data', handleUserInput);
   return stdin;
-
 }
 
-
+const handleUserInput = function (key) {
+  if (key === '\u0003'){
+    process.exit(0);
+  }
+  if (key === 'w'){
+    connection.write("Move: up");
+  }
+  if (key === 'a'){
+    connection.write("Move: left");
+  }
+  if (key === 's'){
+    connection.write("Move: down");
+  }
+  if (key === 'd'){
+    connection.write("Move: right");
+  }
+  if (key === 'y') {
+    connection.write("Say: GONNA GET CHA :D");
+  }
+}
 module.exports = {
   setupInput,
-
 }
--- play.js
+++ play.js
@@ -1,15 +1,10 @@
-const net = require("net");
-const client = require("./client");
-const input = require("./input");
+const {connect} = require("./client");
+const {setupInput} = require("./input");
 
-
-client.connect();
-input.setupInput();
-
+let conn = connect();
+setupInput(conn);
 
 
 
-// "Move: up" - move up one square (unless facing down)
-// "Move: down" - move down one square (unless facing up)
-// "Move: left" - move left one square (unless facing right)
-// "Move: right" - move left one square (unless facing left)
+
+
